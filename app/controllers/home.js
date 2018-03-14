43d54831fc1569d50da75a399d82b56042e2fc19
jolugbo <jolugbofemi@gmail.com>
2018-03-14 23:08:37 +0100

    commit made after npm installing busboy and creating GUI to upload to azure

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const Busboy = require('busboy');
+const blobStorage = require('../services/blobStorage');
 const Article = require('../models/article');
-const Config = require('../../config/config')
+const Config = require('../../config/config');
 
 module.exports = (app) => {
   app.use('/', router);
 };
 
 router.get('/', (req, res, next) => {
-  throw new Error("Failed!!");
+  //throw new Error("Failed!!");
   const articles = [new Article(), new Article()];
   res.render('index', {
     title: Config.app.name,
     articles: articles
+  });
+});
+router.post('/upload', function(req,res){
+  var busboy = new Busboy({headers:req.headers});
+
+  busboy.on('file',function(fieldname,file,filename,encoding,mimetype){
+    blobStorage.saveToBlob(filename,file,function(err,result){
+      if (err) {
+        res.send(500,err);
+      }
+      else{
+        res.redirect('/show?name=' + encodeURI(filename));
+      }
+    });
+  });
+  req.pipe(busboy);
+});
+router.get('/show',function(req,res){
+  res.render('show',{
+    name:req.query.name,
+    url: blobStorage.getUrl(req.query.name)
   });
 });
---
+++ app/services/blobStorage.js
@@ -0,0 +1,13 @@
+process.env['AZURE_STORAGE_ACCOUNT'] = '';
+process.env['AZURE_STORAGE_ACCESS_KEY'] = '';
+
+function saveToBlob(name,stream,cb){
+ 
+}
+function getUrl(name){
+
+}
+module.exports={
+    saveToBlob: saveToBlob,
+    getUrl: getUrl
+}
